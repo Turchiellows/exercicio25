8f1d2a6ea4402f9aadc2db22b26306bb9278cff1
Turchiellows <>
2024-09-28 14:32:49 -0300

    refactor: Mudança de indentação

--- src/main/java/Exercicio25.java
+++ src/main/java/Exercicio25.java
@@ -1,34 +1,28 @@
+// IMPORTS
 
 import javax.swing.JOptionPane;
 
-
 public class Exercicio25 {
 
-	public static void main(String[] args) {
-		// CONFIG
-		int qtde = 20;
-		
-		
-		
-		// INPUT
-		int vetor[] = new int[qtde];
-		for (int i = 0; i < qtde; i++) {
-			vetor[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite o número :" + i));
-		}
-		
-		
-		
-		// PROCESS
-		int soma = 0;
-		for (int i = 0; i < qtde; i++) {
-			soma = soma + vetor[i];
-		}
-		
-		double media = soma / qtde;
-		
-		
-		
-		// OUTPUT
-		JOptionPane.showMessageDialog(null, "A soma é: " + soma + "\nA média é " + media);
-	}
+   public static void main(String[] args) {
+      // CONFIG
+      int qtde = 20;
+
+      // INPUT
+      int vetor[] = new int[qtde];
+      for (int i = 0; i < qtde; i++) {
+         vetor[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite o número :" + i));
+      }
+
+      // PROCESS
+      int soma = 0;
+      for (int i = 0; i < qtde; i++) {
+         soma = soma + vetor[i];
+      }
+
+      double media = soma / qtde;
+
+      // OUTPUT
+      JOptionPane.showMessageDialog(null, "A soma é: " + soma + "\nA média é " + media);
+   }
 }
